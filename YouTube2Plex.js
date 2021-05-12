07b9653aa6b649ca738da51092cb710a201539ba
MIXERRULES <9809371+mixerrules@users.noreply.github.com>
2021-05-12 13:39:25 -0500

    Major filesystem management overhaul

--- YouTube2Plex.js
+++ YouTube2Plex.js
@@ -1,20 +1,24 @@
 const path = require('path');
+const https = require('https');
 const fs = require('fs-extra');
 const bp = require('body-parser');
 const ytdl = require('ytdl-core');
 const express = require('express');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+ffmetadata = require("ffmetadata");
 
 const app = express();
 const port = 818;
 
 var plexVideoFolder = ''; // FULL PATH to Plex Video content folder
 var plexMusicFolder = ''; // FULL PATH to Plex Audio content folder
-var convertedVideos = path.join(__dirname, 'converted', 'Videos/');
+var enableLogging = '0'; // 1 or 0 - Easy Logging for making reports on the github
+
+var convertedVideos = path.join(__dirname, 'converted', 'videos/');
 var convertedAudio = path.join(__dirname, 'converted', 'audio/');
 var convertedFolder = path.join(__dirname, 'converted');
-// var enableLogging = '';  1 or 0 - Easy Logging for making reports on the github
+
 
 app.use(bp.json())
 app.use(bp.urlencoded({
@@ -23,83 +27,155 @@ app.use(bp.urlencoded({
 
 app.post('/download', async function(req, res) {
 
-  var youtubelink = req.body.link + "";
-  var filetype = req.body.type + "";
-  var info = await ytdl.getInfo(youtubelink);
+  const youtube = req.body.link + "";
+  const filetype = req.body.type + "";
 
-//  console.log(req.body);
-//  console.log(filetype);
-//  console.log(youtubelink);
-//  console.log(info.videoDetails.title);
+  var youtubelink = "";
+  await changeURL();
+
+  const info = await ytdl.getInfo(youtubelink);
+  const videoID = `${info.videoDetails.videoId}`;
+  const title = `${info.videoDetails.title}`;
+  const description = `${info.videoDetails.description}`;
+  const author = `${info.videoDetails.author.name}`;
+  var thumbnail = '';
+
+  var fixedtitle = '';
+  await fixTitle();
+
+  var thumbnaillink = "";
+  await thumbnailCheck();
+
+  if (enableLogging === "1") {
+    console.log(' ');
+    console.log('=============');
+    console.log(req.body);
+    console.log('Entered link: ' + youtubelink);
+    console.log('Filetype: ' + filetype);
+    console.log('Fixed File Title: ' + fixedtitle);
+    console.log(' ');
+    console.log('--- Video Info: ---');
+    console.log('VideoID: ' + videoID)
+    console.log('Author: ' + author)
+    console.log('Video Title: ' + title)
+    console.log('Description: ' + description)
+    console.log(' ');
+    console.log('--- raw info dump --- ');
+    console.log(info);
+    console.log(' ');
+    console.log('=============');
+    console.log(' ');
+  };
 
   if (filetype === "mp3") {
-    ytdl(youtubelink, {
-      filter: 'audioonly',
-      quality: 'highestaudio'
-    }.pipe(fs.createWriteStream(`${info.videoDetails.title}.mp3`)))
-    .on('progress', function(progress) {
-      console.log('Conversion: ' + Math.round(100*progress.percent)/100 + '% done');
-    })
-    .on('end', function(err) {
-      if (!err)
-        console.log(`=======\n${info.videoDetails.title} Audio downloaded successfully\n=======\n`);
-    })
+
+    /*  ytdl(youtubelink, {
+          filter: 'audioonly',
+          quality: 'highestaudio'
+        }.pipe(fs.createWriteStream(`${info.videoDetails.title}.mp3`)))
+        .on('progress', function(progress) {
+          console.log('Conversion: ' + Math.round(100 * progress.percent) / 100 + '% done');
+        })
+        .on('end', function(err) {
+          if (!err)
+            console.log(`=======\n${info.videoDetails.title} Audio downloaded successfully\n=======\n`);
+        })
+        */
 
     setTimeout(moveMP3, 15000);
 
-    function moveMP3(){
+    function moveMP3() {
       fs.move(`${info.videoDetails.title}.mp3`, convertedAudio, err => {
         if (err) return console.error(err)
         console.log(`======\n${info.videoDetails.title}.mp3 downloaded successfully!\n=======\n`)
       })
-    }
-  }
+    };
+  };
 
   if (filetype === "mp4") {
-    ytdl(youtubelink, {
-      quality: 'highestvideo'
-    }).pipe(fs.createWriteStream(`${info.videoDetails.title}.mp4`));
+    console.log("starting download of " + title + '\n');
+    ytdl(youtubelink, {quality: 'highestvideo'}).pipe(fs.createWriteStream(`${fixedtitle}.mp4`));
 
-    ytdl(youtubelink, {
-      filter: 'audioonly',
-      quality: 'highestaudio'
-    }).pipe(fs.createWriteStream(`${info.videoDetails.title}.mp3`));
+    ytdl(youtubelink, {filter: 'audioonly',quality: 'highestaudio'  }).pipe(fs.createWriteStream(`${fixedtitle}.mp3`));
 
-    setTimeout(makethissleep, 15000);
+    setTimeout(runffmpeg, 15000);
 
-    function makethissleep(){
-      new ffmpeg(`${info.videoDetails.title}.mp4`)
-        .setFfmpegPath(ffmpegPath)
-        .addInput(`${info.videoDetails.title}.mp3`)
-        .output(convertedVideos + `${info.videoDetails.title}.mp4`)
+    function runffmpeg() {
+      console.log("ffmpeg function called");
+      console.log('Fixed File Title: ' + fixedtitle);
+      let newFfmpeg = new ffmpeg(`${fixedtitle}.mp4`);
+      newFfmpeg.setFfmpegPath(ffmpegPath)
+      newFfmpeg.addInput(`${fixedtitle}.mp3`)
+      newFfmpeg.output(convertedVideos + `${fixedtitle} [${info.videoDetails.author.name}].mp4`)
+      newFfmpeg.on('progress', function(progress) {
+        console.log('Combinding Files: ' + Math.round(100 * progress.percent) / 100 + '% done');
+      })
+      newFfmpeg.on('end', function(err) {
+        if (!err)
+          console.log(`=======\n${info.videoDetails.title} Converted Successfully\n`);
+        // Code to Delete only audio MP3
+        fs.unlink(fixedtitle + `.mp3`, function(err) {
+          if (err) throw err;
+          if (enableLogging === "1") {
+            console.log('Temp MP3 deleted!');
+          }
+        });
+        // Code to Delete MP4 with no audio
+        fs.unlink(fixedtitle + `.mp4`, function(err) {
+          if (err) throw err;
+          if (enableLogging === "1") {
+            console.log('Temp MP4 deleted!');
+          }
+        });
+        console.log(`=======\n`);
 
-        .on('progress', function(progress) {
-          console.log('Conversion: ' + Math.round(100*progress.percent)/100 + '% done');
-        })
-
-        .on('end', function(err) {
-          if (!err)
-            console.log(`=======\n${info.videoDetails.title} Converted Successfully\n=======\n`);
-            // Delete only audio MP3
-            fs.unlink(`${info.videoDetails.title}.mp3`, function (err) {
-              if (err) throw err;
-              console.log('Temp MP3 deleted!');
-            });
-            // Code to Delete MP4 with no audio
-            fs.unlink(`${info.videoDetails.title}.mp4`, function (err) {
-              if (err) throw err;
-              console.log('Temp MP4 deleted!');
-            });
-
-
-        })
-        .on('error', function(err) {
-          console.log('error: ' + err);
-        }).run()
-    }
-
+      /*  var data = {
+          author: `${info.videoDetails.author.name}`,
+          title: title,
+          description: description,
+          attachments: ["thumbnail.jpeg"],
+        };
+        ffmetadata.write(convertedVideos + fixedtitle + ` | [${info.videoDetails.author.name}].mp4`, data, function(err) {
+          if (err) console.error("Error writing metadata", err);
+          else console.log("Data written");
+          fs.unlink(`thumbnail.jpeg`, function(err) {
+            if (err) throw err;
+            if (enableLogging === "1") {
+              console.log('Temp Thumbnail deleted!');
+            }
+          });
+        }); */
+      })
+      newFfmpeg.on('error', function(err) {
+        console.log('error: ' + err);
+      })
+      newFfmpeg.run();
+    };
   } else {
     // catch error if unchecked
+  };
+
+  function fixTitle() {
+    var currentTitle = `${info.videoDetails.title}`;
+    const regex = /[<>:"-/\|?*]/gi;
+    fixedtitle = currentTitle.replace(regex, '');
+  };
+
+  function thumbnailCheck() {
+    let thumbnaillink = `https://img.youtube.com/vi/` + videoID + `/maxresdefault.jpg`;
+    var file = fs.createWriteStream('thumbnail.jpeg');
+
+    var request = https.get(thumbnaillink, function(response) {
+      response.pipe(file);
+      file.on('finish', function() {});
+    })
+  };
+
+  function changeURL() {
+    var urlcheck = youtube;
+    let regex = /youtube.com\/watch.v=.........../g;
+    let regexlink = urlcheck.match(regex);
+    youtubelink = "https://www." + regexlink;
   };
 
   res.redirect(301, 'panel');
@@ -115,11 +191,11 @@ app.get('/MoveMP3s', function(req, res) {
   res.redirect(301, 'panel');
 });
 
-app.get('/MoveMP4s', function(req, res){
+app.get('/MoveMP4s', function(req, res) {
 
   fs.move(convertedVideos, plexVideoFolder, err => {
     if (err) return console.error(err)
-      console.log('======\nMP4s moved successfully!\n=======\n')
+    console.log('======\nMP4s moved successfully!\n=======\n')
   })
 
   res.redirect(301, 'panel');
@@ -132,8 +208,8 @@ app.listen(port, () => {
 });
 
 // checks for afew required folders and makes them if they dont already exist
-if (!fs.existsSync(convertedFolder)){
-    fs.mkdirSync(convertedFolder);
-    fs.mkdirSync(convertedVideos);
-    fs.mkdirSync(convertedAudio);
+if (!fs.existsSync(convertedFolder)) {
+  fs.mkdirSync(convertedFolder);
+  fs.mkdirSync(convertedVideos);
+  fs.mkdirSync(convertedAudio);
 }
